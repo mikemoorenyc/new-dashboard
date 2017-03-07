cc249fc1dcfa5b1529625595d6adb4023adc3925
Mike Moore <tigerhollowpoint@gmail.com>
2017-03-07 17:37:07 -0500

    remove button handlers

--- todo/scripts/main-list.js
+++ todo/scripts/main-list.js
@@ -13,12 +13,6 @@ Vue.component('main-list', {
   methods: {
     updateSwipe:function(id) {
       this.swiped = id;
-    },
-    editClick: function(id) {
-      this.$emit('editing', id);
-    },
-    deleteClick: function(id) {
-      App.bus.$emit('delete-item',id)
     }
   }
 
