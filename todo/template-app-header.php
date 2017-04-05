8358f472a7230b6a718720b625e00cf2e0b120ed
Mike Moore <tigerhollowpoint@gmail.com>
2017-04-04 20:12:32 -0400

    Update template-app-header.php

--- todo/template-app-header.php
+++ todo/template-app-header.php
@@ -1,7 +1,7 @@
 <form id="app-header"  @submit.prevent="addClick">
 <div style="display:none;"class="greeting">Hi, {{userInfo.firstname}}</div>
 
-  <input required type="text" id="add_text" value="" placeholder="Add a new to-do item" v-model="title"/>
+  <input @blur="addClick" required type="text" id="add_text" value="" placeholder="Add a new to-do item" v-model="title"/>
   <button  class="icon-button" :class="{active:title.length>0}">
     <span style="width:0;height:0; position:absolute; left:-9999px;">Submit</span>
     <?php include 'icon-add.php';?>
