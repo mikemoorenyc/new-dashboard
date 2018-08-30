ef2a94b630ebf48df8c8d8a561399c63123cc1e6
Mike Moore <tigerhollowpoint@gmail.com>
2018-08-30 09:13:01 -0400

    Update listretriever.php

--- listretriever.php
+++ listretriever.php
@@ -1,6 +1,35 @@
 <?php
 require_once("../../../wp-load.php");
 
+
+if($_GET['ts']) {
+	$args = array(
+    'post_type' 		=> 'todo',
+    'orderby' 			=> 'modified',
+    'order' 			=> 'DESC',
+    'posts_per_page' => -1
+  );
+$files_in_cat_query = new WP_Query($args);
+
+if ( $files_in_cat_query->have_posts() ) {
+	$first = $files_in_cat_query->get_posts()[0];
+	$date = new DateTime($first->post_modified_gmt,new DateTimeZone('UTC'));
+	$date->setTimezone(new DateTimeZone('EST'));
+	
+	if($date->getTimestamp() < intval($_GET['ts'])) {
+		header('HTTP/1.0 304 Not Modified');
+		die();
+
+	}	
+		
+}
+	
+	
+	
+}
+
+
+
 echo json_encode(
   array(
     'status'=>'success',
